5a492176f4d9fd53a7688a08f2497bb477c79f91
SAAC7 <saulabraham881@gmail.com>
2023-11-15 02:07:13 -0600

    intento de que funcione angular

--- linkusac/src/app/app-routing.module.ts
+++ linkusac/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShowComponent } from './Usuario/show/show.component';
+import { AddComponent } from './Usuario/add/add.component';
+import { EditComponent } from './Usuario/edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'show',component:ShowComponent},
+  {path:'add',component:AddComponent},
+  {path:'edit',component:EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- linkusac/src/app/app.component.ts
+++ linkusac/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'linkusac';
+
+  constructor(private router:Router){
+
+  }
+
+  Show(){
+    this.router.navigate(["show"]);
+  }
+  Add(){
+    this.router.navigate(["add"]);
+  }
 }
